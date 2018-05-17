66b4a83ef1ef5db96128da6330de45b192e7349f
qinshouzhi <qinshouzhi@qq.com>
2018-05-17 09:55:40 +0800

    msg

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-    @RequestMapping("/")
+	
+	@Autowired
+	BeanInject bean;
+	
+	@Autowired
+	RedisSettings  redis;
+	
+	@Autowired
+	MyConfig config;
+	
+    @RequestMapping("/hello")
     String home() {
+    	System.out.println("==bean===:"+bean.getAnother());
+    	System.out.println("==redis==:"+redis.getHost());
+    	System.out.println("==config==:"+config.getServers().get(0));
         return "Hello World!";
     }
 }
